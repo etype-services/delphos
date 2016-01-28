ea2f92c97448ea994d90d887d815da81f80f1ea0
etypeweb <etypeweb@delphos>
2016-01-28 17:13:24 +0000

    initial commit

---
+++ sites/sites.php
@@ -0,0 +1,6 @@
+<?php
+$sites['thefortpolkguardian.etypegoogle6.com'] = 'thefortpolkguardian.com';
+$sites['thefortpolkguardian.com'] = 'thefortpolkguardian.com';
+$sites['www.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
+$sites['prod.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
+$sites['dev.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
