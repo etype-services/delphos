a11fce4774588a0c6ab4572b97a0ec2ed5524f64
etypeweb <etypeweb@delphos>
2016-02-24 21:16:23 +0000

    update

--- sites/sites.php
+++ sites/sites.php
@@ -1,6 +1,13 @@
 <?php
+
 $sites['thefortpolkguardian.etypegoogle6.com'] = 'thefortpolkguardian.com';
 $sites['thefortpolkguardian.com'] = 'thefortpolkguardian.com';
 $sites['www.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
 $sites['prod.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
 $sites['dev.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
+
+$sites['wausharaargus.etypegoogle6.com'] = 'wausharaargus.com';
+$sites['wausharaargus.com'] = 'wausharaargus.com';
+$sites['www.wausharaargus.com'] = 'wausharaargus.com';
+$sites['prod.wausharaargus.com'] = 'wausharaargus.com';
+$sites['dev.wausharaargus.com'] = 'wausharaargus.com';
