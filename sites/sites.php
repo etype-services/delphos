cc605660fd2c24002752a0267619cde6120b69e7
Charlie Llewellin <charliellewellin@gmail.com>
2016-05-12 07:27:40 -0500

    sabine index

--- sites/sites.php
+++ sites/sites.php
@@ -5,49 +5,33 @@ $sites['test.etypegoogle6.com'] = 'test.etypegoogle6.com';
 $sites['thefortpolkguardian.etypegoogle6.com'] = 'thefortpolkguardian.com';
 $sites['thefortpolkguardian.com'] = 'thefortpolkguardian.com';
 $sites['www.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
-$sites['prod.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
-$sites['dev.thefortpolkguardian.com'] = 'thefortpolkguardian.com';
 
 $sites['wausharaargus.etypegoogle6.com'] = 'wausharaargus.com';
 $sites['wausharaargus.com'] = 'wausharaargus.com';
 $sites['www.wausharaargus.com'] = 'wausharaargus.com';
-$sites['prod.wausharaargus.com'] = 'wausharaargus.com';
-$sites['dev.wausharaargus.com'] = 'wausharaargus.com';
 
 $sites['natchitochestimes.etypegoogle6.com'] = 'natchitochestimes.com';
 $sites['natchitochestimes.com'] = 'natchitochestimes.com';
 $sites['www.natchitochestimes.com'] = 'natchitochestimes.com';
-$sites['prod.natchitochestimes.com'] = 'natchitochestimes.com';
-$sites['dev.natchitochestimes.com'] = 'natchitochestimes.com';
 
 $sites['cordellbeacon.etypegoogle6.com'] = 'cordellbeacon.com';
 $sites['cordellbeacon.com'] = 'cordellbeacon.com';
 $sites['www.cordellbeacon.com'] = 'cordellbeacon.com';
-$sites['prod.cordellbeacon.com'] = 'cordellbeacon.com';
-$sites['dev.cordellbeacon.com'] = 'cordellbeacon.com';
 
 $sites['dublincitizen.etypegoogle6.com'] = 'dublincitizen.com';
 $sites['dublincitizen.com'] = 'dublincitizen.com';
 $sites['www.dublincitizen.com'] = 'dublincitizen.com';
-$sites['prod.dublincitizen.com'] = 'dublincitizen.com';
-$sites['dev.dublincitizen.com'] = 'dublincitizen.com';
 
 $sites['madillrecord.etypegoogle6.com'] = 'madillrecord.net';
 $sites['madillrecord.net'] = 'madillrecord.net';
 $sites['www.madillrecord.net'] = 'madillrecord.net';
-$sites['prod.madillrecord.net'] = 'madillrecord.net';
-$sites['dev.madillrecord.net'] = 'madillrecord.net';
 
 $sites['mcbeacon.etypegoogle6.com'] = 'mcbeacon.com';
-$sites['dev.mcbeacon.com'] = 'mcbeacon.com';
-$sites['prod.mcbeacon.com'] = 'mcbeacon.com';
 $sites['www.mcbeacon.com'] = 'mcbeacon.com';
 $sites['mcbeacon.com'] = 'mcbeacon.com';
 
 /* vcnewsreview.etypegoogle6.com */
 $sites['vcnewsreview.etypegoogle6.com'] = 'vcnewsreview.com';
-$sites['dev.vcnewsreview.com'] = 'vcnewsreview.com';
-$sites['prod.vcnewsreview.com'] = 'vcnewsreview.com';
 $sites['www.vcnewsreview.com'] = 'vcnewsreview.com';
 $sites['vcnewsreview.com'] = 'vcnewsreview.com';
 
@@ -56,29 +40,24 @@ $sites['vilascountynewsreview.com'] = 'vcnewsreview.com';
 
 /* beardstownnewspapers.etypegoogle6.com */
 $sites['beardstownnewspapers.etypegoogle6.com'] = 'beardstownnewspapers.com';
-$sites['dev.beardstownnewspapers.com'] = 'beardstownnewspapers.com';
-$sites['prod.beardstownnewspapers.com'] = 'beardstownnewspapers.com';
 $sites['www.beardstownnewspapers.com'] = 'beardstownnewspapers.com';
 $sites['beardstownnewspapers.com'] = 'beardstownnewspapers.com';
 
 $sites['bulletintimesnews.etypegoogle6.com'] = 'bulletintimesnews.com';
-$sites['dev.bulletintimesnews.com'] = 'bulletintimesnews.com';
-$sites['prod.bulletintimesnews.com'] = 'bulletintimesnews.com';
 $sites['www.bulletintimesnews.com'] = 'bulletintimesnews.com';
 $sites['bulletintimesnews.com'] = 'bulletintimesnews.com';
 
 $sites['thegearystar.etypegoogle6.com'] = 'thegearystar.com';
-$sites['dev.thegearystar.com'] = 'thegearystar.com';
-$sites['prod.thegearystar.com'] = 'thegearystar.com';
 $sites['www.thegearystar.com'] = 'thegearystar.com';
 $sites['thegearystar.com'] = 'thegearystar.com';
 
 /* thesabineindex.etypegoogle6.com */
 $sites['thesabineindex.etypegoogle6.com'] = 'thesabineindex.com';
-$sites['dev.thesabineindex.com'] = 'thesabineindex.com';
-$sites['prod.thesabineindex.com'] = 'thesabineindex.com';
 $sites['www.thesabineindex.com'] = 'thesabineindex.com';
 $sites['thesabineindex.com'] = 'thesabineindex.com';
 
 $sites['www.sabinetoday.com'] = 'thesabineindex.com';
 $sites['sabinetoday.com'] = 'thesabineindex.com';
+
+$sites['sabineindex.net'] = 'thesabineindex.com';
+$sites['www.sabineindex.net'] = 'thesabineindex.com';
