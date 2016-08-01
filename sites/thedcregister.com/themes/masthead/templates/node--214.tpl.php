12240aacb1e048e00911f8dfd7f0a40b0d70e2c2
root <root@delphos.c.modern-photon-112115.internal>
2016-08-01 15:58:15 +0000

    updates and thedcregister

---
+++ sites/thedcregister.com/themes/masthead/templates/node--214.tpl.php
@@ -0,0 +1,34 @@
+<form name="form1" method="POST" action="" enctype="multipart/form-data">
+      <fieldset>
+               
+                
+            <div>
+<h3 style="background:gray;line-height: 2.0em;font-size: 16px;"><center>Select Publication</center></h3>
+<br/>
+<hr>
+              <!-- <input type="radio" name="publication" value="255" required>Morgan City Daily-Review<br>
+         <input type="radio" name="publication" value="256">St. Mary and Franklin Banner Tribune<br>--><br/>
+         <ul>
+         <li style="list-style-type: square;list-style-position: inside;">
+         <a href="http://etypeservices.com/Register%20PublicationsID520/default.aspx?PubID=2399" Style="font-size: large;" >Dearborn County Register</a></li><br/>
+         <li style="list-style-type: square;list-style-position: inside;">
+         <a href="http://etypeservices.com/Register%20PublicationsID520/default.aspx?PubID=2402" Style="font-size: large;" >Journal Press</a><br></li>
+         </ul>
+            </div>
+
+            
+ 
+ <br/>
+            
+ 
+        
+ 
+            
+ 
+ 
+            
+            </fieldset>
+         <!--       
+        <input type="submit" name="submit" value="Submit!" style="background-color: gainsboro;border-radius: 4px;width: 93px;height: 28px;border: 1px solid #CCC;text-decoration: none;color: #000;text-shadow: white 0 1px 1px;padding: 2px;">-->
+                      
+        </form>
