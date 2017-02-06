6e272a9ed5dc8d3479feb9453ca7dd051bd659a1
Charlie Llewellin <charlie@llewellin.net>
2017-02-06 10:40:59 -0600

    drupal 7.54, ckeditor, themes

--- sites/all/libraries/ckeditor/config.js
+++ sites/all/libraries/ckeditor/config.js
@@ -1,11 +1,10 @@
-﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
