85363f18ce4d13a347fac8c44fe86f59b85bd433
etypeweb <etypeweb@delphos>
2021-05-04 02:01:21 +0000

    automatic updates

--- sites/sites.php
+++ sites/sites.php
@@ -1,15 +1,7 @@
 <?php
-
-$sites['etypegoogle6.com'] = 'thedcregister.etypegoogle6.com.delete';
-$sites['wausharaargus.etypegoogle6.com'] = 'wausharaargus.com';
-$sites['wausharaargus.com'] = 'wausharaargus.com';
-$sites['www.wausharaargus.com'] = 'wausharaargus.com';
-
-$sites['archive.dublincitizen.com'] = 'archive.dublincitizen.etypegoogle6.com';
-
 $sites['www.mcbeacon.com'] = 'mcbeacon.etypegoogle6.com';
-$sites['archive.mcbeacon.com'] = 'archive.mcbeacon.etypegoogle6.com';
-$sites['mcbeaconarchives.etypegoogle6.com'] = 'mcbeaconarchives';
+//$sites['archive.mcbeacon.com'] = 'archive.mcbeacon.etypegoogle6.com';
+//$sites['mcbeaconarchives.etypegoogle6.com'] = 'mcbeaconarchives';
 
 /* beardstownnewspapers.etypegoogle6.com */
 $sites['beardstownnewspapers.etypegoogle6.com'] = 'beardstownnewspapers.com';
@@ -20,8 +12,6 @@ $sites['beardstownnewspapersarchives.etypegoogle6.com'] = 'beardstownnewspapersa
 $sites['bulletintimesnews.etypegoogle6.com'] = 'bulletintimesnews.com';
 $sites['www.bulletintimesnews.com'] = 'bulletintimesnews.com';
 $sites['bulletintimesnews.com'] = 'bulletintimesnews.com';
-
-$sites['archive.thegearystar.com'] = 'archive.thegearystar.etypegoogle6.com';
 
 /* thesabineindex.etypegoogle6.com */
 $sites['thesabineindex.etypegoogle6.com'] = 'thesabineindex.com';
@@ -38,9 +28,6 @@ $sites['www.sabineindex.net'] = 'thesabineindex.com';
 $sites['mansfieldenterprise.etypegoogle6.com'] = 'mansfieldenterprise.com';
 $sites['www.mansfieldenterprise.com'] = 'mansfieldenterprise.com';
 $sites['mansfieldenterprise.com'] = 'mansfieldenterprise.com';
-
-/* lambcountyleadernews.etypegoogle6.com */
-$sites['archive.lambcountyleadernews.com'] = 'archive.lambcountyleadernews.etypegoogle6.com';
 
 /* winnparishenterprise.etypegoogle6.com */
 $sites['winnparishenterprise.etypegoogle6.com'] = 'winnparishenterprise.etypegoogle6.com';
